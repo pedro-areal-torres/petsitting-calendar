d6d131f0da39cfb1569578cff1b44251ae6e1733
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2025-04-28 22:19:22 +0200

    chore: drizzle setup

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,3 +1,9 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,43 @@
-import { pgTable, varchar, date, uuid, text, time, pgEnum } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { pgTable, varchar, date, time, pgEnum, serial } from 'drizzle-orm/pg-core';
 
-export const availabilityStatus = pgEnum('availability_status', ['unavailable', 'unknown']);
-
-export const availability = pgTable('availability', {
-  date: date('date').primaryKey(),
-  status: availabilityStatus('status').notNull(),
-});
+export const eventStatus = pgEnum('event_status', ['unavailable', 'unknown', 'booked']);
+export const petType = pgEnum('pet_type', ['dog', 'cat']);
 
 export const pets = pgTable('pets', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: serial('id').primaryKey(),
   name: varchar('name', { length: 100 }).notNull(),
-  imageUrl: text('image_url'),
+  type: petType('type').notNull().default('dog'),
 });
 
-export const petsittingEvents = pgTable('petsitting_events', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  date: date('date').notNull(),
-  petId: uuid('pet_id').references(() => pets.id, { onDelete: 'set null' }),
+export const events = pgTable('events', {
+  date: date('date').primaryKey(),
+  status: eventStatus('status').notNull(),
+});
+
+export const bookings = pgTable('bookings', {
+  id: serial('id').primaryKey(),
   startTime: time('start_time').notNull(),
   endTime: time('end_time').notNull(),
-  notes: text('notes'),
+  petId: serial('pet_id')
+    .references(() => pets.id, { onDelete: 'set null' })
+    .notNull(),
+  eventDate: date('event_date')
+    .references(() => events.date, { onDelete: 'set null' })
+    .notNull(),
 });
+
+export const eventsRelations = relations(events, ({ many }) => ({
+  bookings: many(bookings),
+}));
+
+export const bookingsRelations = relations(bookings, ({ one }) => ({
+  pet: one(pets, {
+    fields: [bookings.petId],
+    references: [pets.id],
+  }),
+  event: one(events, {
+    fields: [bookings.eventDate],
+    references: [events.date],
+  }),
+}));
